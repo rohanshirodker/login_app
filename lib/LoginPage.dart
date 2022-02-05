ed5292d08e51b2b6380136cdcbe475d3dcbea1dd
Rohan <rohanshirodker@gmail.com>
2022-02-06 03:23:28 +0530

    firebase login Authentication and receve data

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,85 +1,168 @@
-
-import 'package:cyanodoc_test/auth.dart';
+import 'dart:ui';
+import 'package:cyanodoc_test/ProfilePage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'ProfilePage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 
 
 
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  //firebase variable
+  final _auth = FirebaseAuth.instance;
 
 
+
+  // editing controller
   final TextEditingController emailController = new TextEditingController();
   final TextEditingController passwordController = new TextEditingController();
 
 
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login page'),
+
+    //email
+    final emailField = TextFormField(
+        autofocus: false,
+        controller: emailController,
+        keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please Enter Your Email");
+        }
+        // reg expression for email validation
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+            .hasMatch(value)) {
+          return ("Please Enter a valid email");
+        }
+        return null;
+      },
+      onSaved: (value){
+          emailController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+        prefixIcon : Icon(Icons.mail,color: Colors.black),
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Email",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
-       child: Form(
-         child: Column(
-           children: <Widget>[
-             SizedBox(height: 20.0),
-             TextFormField(
-               onChanged: (val){
-                 setState(()=> email = val);
-
-               },
-             ),
-             SizedBox(height: 20.0),
-             TextFormField(
-               obscureText: true,
-               onChanged: (val){
-                 setState(()=> pass = val);
-
-               },
-             ),
-
-             ElevatedButton(
-               child: Text('sign in'),
-               onPressed: () async{
-                 dynamic result = await _auth.signInAnon();
-                 if(result == null ){
-                   print('error signing in');
-                 }
-                 else{
-                   Navigator.push(
-                     context,
-                     MaterialPageRoute(builder: (context) => ProfilePage()),
-                   );
-                 }
-               },
-             ),
-
-           ],
-         ),
-       ),
-
-
-
-
-
-
-
-
-
-
-
-
-    )
     );
+
+    //password
+    final passwordField = TextFormField(
+      autofocus: false,
+      controller: passwordController,
+      obscureText: true,
+      validator: (value) {
+        RegExp regex = new RegExp(r'^.{6,}$');
+        if (value!.isEmpty) {
+          return ("Password is required for login");
+        }
+        if (!regex.hasMatch(value)) {
+          return ("Enter Valid Password(Min. 6 Character)");
+        }
+      },
+      onSaved: (value){
+        passwordController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+        prefixIcon : Icon(Icons.vpn_key,color: Colors.black),
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Password",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+
+// button
+    final loginButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color : Colors.deepPurple,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20,15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+
+        onPressed: (){
+          signIn(emailController.text, passwordController.text);
+        },
+        child: Text("Login",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20, color:Colors.white,fontWeight: FontWeight.bold
+          )
+        )
+      ),
+    );
+
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(10),
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+
+                    emailField,
+                    SizedBox(height: 25),
+
+                    passwordField,
+                    SizedBox(height: 45),
+
+                    loginButton,
+                    SizedBox(height: 45),
+
+                  ],
+              ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+   }
+
+
+    //login function
+    void signIn(String email, String password) async{
+      if(_formKey.currentState!.validate()){
+        await _auth
+            .signInWithEmailAndPassword(email: email, password: password)
+            .then((uid) => {
+           Fluttertoast.showToast(msg: "Login Successful"),
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => ProfilePage() )),
+        }).catchError((e)
+            {
+              Fluttertoast.showToast(msg: e!.message);
+                  });
+
+      }
+    }
   }
-}
+
+
--- lib/ProfilePage.dart
+++ lib/ProfilePage.dart
@@ -1,29 +1,82 @@
-import 'package:cyanodoc_test/auth.dart';
+ import 'dart:ui';
+
+import 'package:cyanodoc_test/LoginPage.dart';
+import 'package:cyanodoc_test/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
 
-class ProfilePage extends StatelessWidget {
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
   @override
 
-  final Authservice _auth = Authservice();
+User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+void initState(){
+  super.initState();
+  FirebaseFirestore.instance
+  .collection("users")
+  .doc(user!.uid)
+  .get()
+  .then((value){
+    this.loggedInUser = UserModel.fromMap(value.data());
+    setState(() {
 
+    });
+  });
+}
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
-            actions: <Widget>[
-              FlatButton.icon(
-                  icon: Icon(Icons.person),
-                  label: Text('logout'),
-              onPressed:() async {
-                    await _auth.signOut();
-
-              },
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Welcome",
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)
               ),
-
-        ],
+              SizedBox(height: 25),
+              Text(
+                  "${loggedInUser.firstName}",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+              ),
+              SizedBox(height: 25),
+              Text(
+                  "${loggedInUser.email}",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+              ),
+              SizedBox(height: 25),
+              ActionChip(label: Text("Logout"),onPressed: (){
+                logout(context);
+              },),
+            ],
+          )
+        ),
       ),
 
     );
   }
+
+
+  Future<void> logout(BuildContext) async
+  {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginPage() ));
+
+  }
+
 }
